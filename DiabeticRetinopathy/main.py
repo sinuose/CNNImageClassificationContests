0706dd69c6c8e0519394323cd269d981465a4eec
Martin J. <34733738+sinuose@users.noreply.github.com>
2025-02-18 15:31:17 -0800

    sample data

--- DiabeticRetinopathy/main.py
+++ DiabeticRetinopathy/main.py
@@ -1,5 +1,9 @@
-
+import kaggle
 # https://www.kaggle.com/c/diabetic-retinopathy-detection/data
 
 if __name__ == "__main__":
+
+    # if the data folder is empty
+    kaggle.api.authenticate()
+
     print('Hello World')
